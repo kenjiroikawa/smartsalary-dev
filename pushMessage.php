529287644a2122c0af213cdedf44377345b7cc55
akamata <akamata@JtrustcNP19.local>
2018-10-03 18:19:55 +0900

    pushMessaget

---
+++ pushMessage.php
@@ -0,0 +1,25 @@
+<?PHP
+
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+
+// あなたのユーザーIDを入力してください。
+$userId = 'ユーザーID'；
+$message = 'Hello Push API';
+
+// メッセージをユーザーID宛にプッシュ
+$response = $bot->pushMessage($userId, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message));
+if(!$response->isSucceeded()){
+  //エラー内容を出力
+  error_log('Failed! '. $response->getHTTPStatus . ' ' .
+                            $response->getRawBody());
+}
+
+?>
